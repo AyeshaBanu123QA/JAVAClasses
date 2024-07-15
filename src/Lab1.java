d4dac714b8b445875199d823972df83814f3ef50
AyeshaBanu123QA <79.ayesha@gmail.com>
2024-07-15 15:34:12 +0530

    Java programs

---
+++ src/Lab1.java
@@ -0,0 +1,8 @@
+public class Lab1 {
+    public static void main(String[] args) {
+        int a = 87;
+        long b = 91;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+    }
+}
